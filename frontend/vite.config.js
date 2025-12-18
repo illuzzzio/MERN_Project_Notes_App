f996ffdbea1940bcd330534b7e9a8792a193af35
Your Name <animusaaa@gmail.com>
2025-12-18 15:20:55 +0530

    tailwind integrated into vite react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  tailwindcss(),],
+  
 })
+// from tailwind docs, i also learned to write in App.css file from the tailwind css docs
